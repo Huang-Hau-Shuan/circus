0a18b23f721722af673a4597e7df36659fe67c57
hau shuan <huanghaushuan@gmail.com>
2026-02-13 17:15:44 +0800

    Revert "Add new animals"
    This reverts commit b56a7ff87eb42ef4690f4b8d01a39a726da591ff.

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -49,64 +49,8 @@ public class Circus {
     }
 
     public static void main(String[] args) {
-        System.out.println("Number of animals in circus: " + animals.length);
-        for (Animal a : animals){
-            System.out.println(a);
-        }
-
-//        animals[3] = new Elephant("Strong one");
-//        System.out.println("Number of animals in circus: " + animals.length);
-//        for (Animal a : animals){
-//            System.out.println(a);
-//        }
-
-        ArrayList<Animal> animalArrayList = new ArrayList<>(Arrays.asList(animals));
-        System.out.println("number of animals in circus (AL): " + animalArrayList.size());
-
-        animalArrayList.add(new Elephant("Strong one"));
-
-//        printAllAnimals(animalArrayList);
-        printNumberOfAnimals(animalArrayList);
-
-        animalArrayList.add(new Duck("Andy"));
-        Tiger sherKhan = new Tiger ("Sher Khan");
-        animalArrayList.add(sherKhan);
-        Parrot bobby = new Parrot("bobby");
-        animalArrayList.add(bobby);
-
-        System.out.println("Position of Sher Khan in the arrayList is " +
-                animalArrayList.indexOf(sherKhan));
-
-        Animal candidate = findAnimal(animalArrayList, "Polly");
-        System.out.println("Polly is at index: " + animalArrayList.indexOf(candidate));
-
-        System.out.println("Before sorting");
-        printAllAnimals(animalArrayList);
-
-        animalArrayList.sort(Animal.AnimalNameComparator);
-        System.out.println("After sorting");
-        printAllAnimals(animalArrayList);
-//        makeAnimalsTalk();
-//        System.out.println("Total value of animals " + calculateAssetValue(animals));
-//        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
-    }
-
-    private static void printAllAnimals(ArrayList<Animal> animalArrayList) {
-        for (Animal a : animalArrayList){
-            System.out.println(a);
-        }
-    }
-    private static void printNumberOfAnimals(ArrayList<Animal> animalArrayList) {
-        System.out.println("number of animals in circus (AL): " + animalArrayList.size());
-    }
-
-    private static Animal findAnimal (ArrayList<Animal> animalArrayList, String name){
-        for (Animal a : animalArrayList){
-            if (a.name.equals(name)){
-                return a;
-            }
-        }
-        return null;
+        makeAnimalsTalk();
+        System.out.println("Total value of animals " + calculateAssetValue(animals));
+        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
     }
 }
-
